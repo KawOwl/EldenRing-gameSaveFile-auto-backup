c80e0eee6756fbff5e93740b6905034f9a06d7d1
DameNeko <phoensxar@gmail.com>
2022-03-05 20:41:07 +0800

    Create StartEldenRingGameSaveFileAutoBackup.ps1

---
+++ StartEldenRingGameSaveFileAutoBackup.ps1
@@ -0,0 +1,47 @@
+$second = 1000
+$minute = 60 * $second
+$sourceIdentifier = "EldenRingSaveBackup"
+$timer = New-Object -TypeName System.Timers.Timer    
+$timer.Interval = 10 * $minute
+$timer.Autoreset = $True
+$timer.Enabled = $True
+
+$objectEventArgs = @{
+  InputObject = $timer
+	EventName = 'Elapsed'
+  SourceIdentifier = $sourceIdentifier
+}
+
+$action = { Backup-Action }
+
+$EventJob = Register-ObjectEvent @objectEventArgs -Action $action
+
+. $EventJob.Module {
+	$SavePath = $HOME + "\AppData\Roaming\EldenRing"
+	$BackupFolderName1 = "\EldenRingSaveAutoBackup"
+	$BackupFolderName2 = "\ERSAB-BB"
+	$BackupFolderPath1 = $HOME + $BackupFolderName1
+	$BackupFolderPath2 = $HOME + $BackupFolderName2
+
+	function Backup-Save-To-Path {
+		param (
+			$destinationFolder
+		)
+
+		If(!(test-path $destinationFolder))
+		{
+			New-Item -ItemType Directory -Force -Path $destinationFolder
+		}
+
+		$now = Get-Date -Format o | ForEach-Object { $_ -replace ":", "-" }
+
+		$destinationPath = $destinationFolder + "/" + $now + ".zip"
+		
+		Compress-Archive -Force -Path $SavePath -DestinationPath $destinationPath
+	}
+
+	function Backup-Action {
+		Backup-Save-To-Path -destinationFolder $BackupFolderPath1
+		Backup-Save-To-Path -destinationFolder $BackupFolderPath2
+	}
+}
